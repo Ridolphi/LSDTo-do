6da25571302f585c652f7b59671296cfef707692
Daniel Vargas García <161933152+DaniVG7@users.noreply.github.com>
2024-06-29 13:58:51 +0200

    taskStatus

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -5,6 +5,7 @@ export default {
             TaskName: 'Misión imposible',
             TaskDescription: 'Hacer inteligente a Adri',
             Department: 'Artist & bookings',
+            Status: 'TO DO',
         }
     }
 }
@@ -15,5 +16,6 @@ export default {
         <div class="text-900 font-medium text-xl">{{ TaskName }}</div>
         <div class="text-500">{{ TaskDescription }}</div>
         <div class="text-500">Department: {{ Department }}</div>
+        <div class="text-500">Status: {{ Status }}</div>
     </div>
 </template>
