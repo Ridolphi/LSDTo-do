535ebf9374656e65efdbdad2ef24e77f5fb89067
Daniel Vargas García <dvg9383@gmail.com>
2024-06-30 03:17:38 +0200

    sidebar

--- src/layout/AppConfig.vue
+++ src/layout/AppConfig.vue
@@ -1,130 +1,63 @@
 <script setup>
-// import AddTask from "../components/AddTask.vue";
 import { ref, computed } from 'vue';
-import { usePrimeVue } from 'primevue/config';
-import { useLayout } from '@/layout/composables/layout';
 
-defineProps({
-    simple: {
-        type: Boolean,
-        default: false
-    }
-});
-
-const $primevue = usePrimeVue();
-const inputStyle = computed(() => $primevue.config.inputStyle || 'outlined');
-
-const scales = ref([12, 13, 14, 15, 16]);
-const visible = ref(false);
-const inputStyles = ref([
-    { label: 'Outlined', value: 'outlined' },
-    { label: 'Filled', value: 'filled' }
-]);
-const menuModes = ref([
-    { label: 'Static', value: 'static' },
-    { label: 'Overlay', value: 'overlay' }
-]);
-const compactMaterial = ref(false);
-const primaryFocusRing = ref(true);
-
-const { setScale, layoutConfig } = useLayout();
-
-const onConfigButtonClick = () => {
-    visible.value = !visible.value;
-};
-const onChangeTheme = (theme, mode) => {
-    $primevue.changeTheme(layoutConfig.theme.value, theme, 'theme-css', () => {
-        layoutConfig.theme.value = theme;
-        layoutConfig.darkTheme.value = mode;
-    });
-};
-const decrementScale = () => {
-    setScale(layoutConfig.scale.value - 1);
-    applyScale();
-};
-const incrementScale = () => {
-    setScale(layoutConfig.scale.value + 1);
-    applyScale();
-};
-const applyScale = () => {
-    document.documentElement.style.fontSize = layoutConfig.scale.value + 'px';
-};
-const onInputStyleChange = (value) => {
-    $primevue.config.inputStyle = value;
-};
-const onMenuModeChange = (value) => {
-    layoutConfig.menuMode.value = value;
-};
-const onRippleChange = (value) => {
-    layoutConfig.ripple.value = value;
-};
-const onDarkModeChange = (value) => {
-    const newThemeName = value ? layoutConfig.theme.value.replace('light', 'dark') : layoutConfig.theme.value.replace('dark', 'light');
-
-    layoutConfig.darkTheme.value = value;
-    onChangeTheme(newThemeName, value);
-};
-const changeTheme = (theme, color) => {
-    let newTheme, dark;
-
-    newTheme = theme + '-' + (layoutConfig.darkTheme.value ? 'dark' : 'light');
-
-    if (color) {
-        newTheme += '-' + color;
-    }
-
-    if (newTheme.startsWith('md-') && compactMaterial.value) {
-        newTheme = newTheme.replace('md-', 'mdc-');
-    }
-
-    dark = layoutConfig.darkTheme.value;
-
-    onChangeTheme(newTheme, dark);
-};
-const isThemeActive = (themeFamily, color) => {
-    let themeName;
-    let themePrefix = themeFamily === 'md' && compactMaterial.value ? 'mdc' : themeFamily;
-
-    themeName = themePrefix + (layoutConfig.darkTheme.value ? '-dark' : '-light');
-
-    if (color) {
-        themeName += '-' + color;
-    }
-
-    return layoutConfig.theme.value === themeName;
-};
-const onCompactMaterialChange = (value) => {
-    compactMaterial.value = value;
-
-    if (layoutConfig.theme.value.startsWith('md')) {
-        let tokens = layoutConfig.theme.value.split('-');
-
-        changeTheme(tokens[0].substring(0, 2), tokens[2]);
-    }
-};
-const onFocusRingColorChange = (value) => {
-    primaryFocusRing.value = value;
-    let root = document.documentElement;
-
-    if (value) {
-        if (layoutConfig.darkTheme.value) root.style.setProperty('--p-focus-ring-color', 'var(--primary-500)');
-        else root.style.setProperty('--p-focus-ring-color', 'var(--primary-500)');
-    } else {
-        if (layoutConfig.darkTheme.value) root.style.setProperty('--p-focus-ring-color', 'var(--surface-500)');
-        else root.style.setProperty('--p-focus-ring-color', 'var(--surface-900)');
-    }
-};
+const departments = [
+  "Artist & bookings",
+  "Gastronomy",
+  "Legal security & control",
+  "Marketing & sponsors",
+  "Ticketing & pre-sale",
+  "Stage & equipment",
+]
 </script>
 
 <template>
     <Sidebar v-model:visible="visible" position="right" class="layout-config-sidebar w-26rem" pt:closeButton="ml-auto">
-        <h2>Add task to do</h2>
-        <form action="" class="bg-red-400 ml-auto w-5">
-            <input type="text" placeholder="Task name">
-            <input type="text" placeholder="Department"> <br>
-            <input type="submit">
+        
+        <div class="p-4 w">
+        <h3 class="text-lg font-semibold mb-4 text-inline">Add a new task...</h3>
+        <i id="spinner" class="pi pi-spin pi-spinner"></i>
+        <div class="mb-4">
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="taskName"
+            type="text"
+            placeholder="Task Name"
+          />
+        </div>
+        <div class="mb-4">
+          <select
+            class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          >
+            <option value="" disabled selected>Select department</option>
+            <option
+              v-for="department in departments"
+              :key="department"
+              :value="department"
+            >
+              {{ department }}
+            </option>
+          </select>
+        </div>
+        <div class="mb-4">
+          <textarea
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="department"
+            type="text"
+            placeholder="Task Description..."
+          ></textarea>
+        </div>
+        <div class="flex items-center justify-end">
+          <button
+            class="bg-green-400 cursor-pointer hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            <i class="pi pi-check text-xl"></i>
 
-        </form>
+          </button>
+        </div>
+      </div>
+
     </Sidebar>
 </template>
 
