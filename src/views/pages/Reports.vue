aed958cc5d5fc0935ed704ef2131781dea6ede52
Daniel Vargas García <161933152+DaniVG7@users.noreply.github.com>
2024-07-11 21:24:21 +0200

    gonsales2.0

--- src/views/pages/Reports.vue
+++ src/views/pages/Reports.vue
@@ -3,11 +3,6 @@
     <div><img src="../../../public/imagenes/reports.jpg" alt=""></div>
 </template>
 <style scoped>
-/* img {
-    width: 70vw;
-    height: 81vh;
-    mask-image: linear-gradient(black 70%, transparent);
-} */
 img {
     width: 70vw;
     height: 81vh;
@@ -16,6 +11,13 @@ img {
         linear-gradient(to right, black 90%, transparent),
         linear-gradient(to left, black 90%, transparent);
     mask-composite: intersect;
-    -webkit-mask-composite: intersect;
+}
+
+@media (max-width: 700px) {
+
+    img {
+        height: 70vh;
+        width: 95vw;
+    }
 }
 </style>
