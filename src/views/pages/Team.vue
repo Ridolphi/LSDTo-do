b99c097e0ad202b663649a8c78f8720962a2c27c
DaniVG7 <dvg9383@gmail.com>
2024-07-15 01:19:15 +0200

    team

--- src/views/pages/Team.vue
+++ src/views/pages/Team.vue
@@ -3,7 +3,46 @@ import { ref, computed } from "vue";
 </script>
 
 <template>
-    <div class="team-page">
-        <h2>OUR TEAM G3</h2>
+    <div class="container m-5 m-auto">
+        <div class="grid">
+            <img src="../../../public/imagenes/cucucucucu.jpeg" alt="Imagen 1">
+            <br>
+            <img src="../../../public/imagenes/EuroSpain.png" alt="Imagen 2">
+            <br><span></span>
+            <img src="../../../public/imagenes/NicoGol.webp" alt="Imagen 3">
+            <br> <span></span>
+            <img src="../../../public/imagenes/nicoLam.jpeg" alt="Imagen 4">
+            
+        </div>
     </div>
 </template>
+<style scoped>
+.team-page {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+}
+
+.grid {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    width: 100vw;
+    max-width: 1000px;
+}
+
+.grid img {
+    width: 325px;
+    height: 200px;
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.grid img:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+</style>
