1e4fb6e9e5f2bf362138f9e99141e4e107dbeac9
DaniVG7 <dvg9383@gmail.com>
2024-07-15 03:54:27 +0200

    teams

--- src/views/pages/Team.vue
+++ src/views/pages/Team.vue
@@ -1,20 +1,22 @@
 <script setup>
 import { ref, computed } from "vue";
+import Carousel from "primevue/carousel";
 </script>
 
 <template>
-    <div class="container m-5 m-auto">
+    
+    <div class="container">
         <div class="grid">
             <img src="../../../public/imagenes/cucucucucu.jpeg" alt="cucucu">
             <br>
             <img src="../../../public/imagenes/EuroSpain.png" alt="euro spain">
             <br><span></span>
-            <img src="../../../public/imagenes/NicoGol.webp" alt="nico gol">
+            <img src="../../../public/imagenes/NicoGol.webp" alt="nico gol" class="mid">
             <br> <span></span>
             <img src="../../../public/imagenes/nicoLam.jpeg" alt="nico y lamine">
             <br>
             <img src="../../../public/imagenes/campeones.png" alt="campeones">
-            
+
         </div>
     </div>
 </template>
@@ -27,11 +29,14 @@ import { ref, computed } from "vue";
 .container {
     display: flex;
     justify-content: center;
+    margin-top: 60px;
+    width: 85vw;
+    margin: auto;
 }
 
-.grid {
+.grid { 
     display: grid;
-    gap: 20px;
+    gap: 5px;
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     width: 100vw;
     max-width: 1000px;
@@ -46,5 +51,11 @@ import { ref, computed } from "vue";
 .grid img:hover {
     transform: scale(1.05);
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+.mid{
+    margin: -40px;
+    margin-left: 10px;
+    width: 500px;
+    z-index: 1000;
 }
 </style>
