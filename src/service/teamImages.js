c6c3fabbff7a7310c5fe7f9721e52f7291c399e8
DaniVG7 <dvg9383@gmail.com>
2024-07-19 13:31:31 +0200

    imgs

--- src/service/teamImages.js
+++ src/service/teamImages.js
@@ -1,40 +1,40 @@
-export const teamService = {
+export const teamImages = {
     getImages() {
         return [
             {
                 itemImageSrc: '/imagenes/campeones.png',
                 thumbnailImageSrc: '/imagenes/campeones.png',
-                alt: 'Image 3'
+                alt: 'campeones'
             },
             {
                 itemImageSrc: '/imagenes/aRgCamp.png',
                 thumbnailImageSrc: '/imagenes/aRgCamp.png',
-                alt: 'Image 1'
+                alt: 'argentina campeona'
             },
             {
                 itemImageSrc: '/imagenes/cucucucucu.jpeg',
                 thumbnailImageSrc: '/imagenes/cucucucucu.jpeg',
-                alt: 'Image 4'
+                alt: 'cucu cucurella'
             },
             {
                 itemImageSrc: '/imagenes/EuroSpain.png',
                 thumbnailImageSrc: '/imagenes/EuroSpain.png',
-                alt: 'Image 5'
+                alt: 'españa eurocopa'
             },
             {
                 itemImageSrc: '/imagenes/argentos.png',
                 thumbnailImageSrc: '/imagenes/argentos.png',
-                alt: 'Image 2'
+                alt: 'argentinos'
             },
             {
                 itemImageSrc: '/imagenes/NicoGol.webp',
                 thumbnailImageSrc: '/imagenes/NicoGol.webp',
-                alt: 'Image 6'
+                alt: 'gol nico williams'
             },
             {
                 itemImageSrc: '/imagenes/nicoLam.jpeg',
                 thumbnailImageSrc: '/imagenes/nicoLam.jpeg',
-                alt: 'Image 7'
+                alt: 'nico y lamine celebrando'
             },
         ];
     }
